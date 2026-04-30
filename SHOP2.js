3d7f6ab3c193747a08d1240679e46ea055669500
Akosh202 <anormurodov681@gmail.com>
2026-04-30 20:47:49 +0500

    Add files via upload

---
+++ SHOP2.js
@@ -0,0 +1,225 @@
+let buy_button1 = document.getElementById("buy_button1");
+let buy_button2 = document.getElementById("buy_button2");
+let buy_button3 = document.getElementById("buy_button3");
+let buy_button4 = document.getElementById("buy_button4");
+let buy_button5 = document.getElementById("buy_button5");
+let buy_button6 = document.getElementById("buy_button6");
+let buy_button7 = document.getElementById("buy_button7");
+let buy_button8 = document.getElementById("buy_button8");
+let buy_button9 = document.getElementById("buy_button9");
+let buy_button10 = document.getElementById("buy_button10");
+let buy_button11 = document.getElementById("buy_button11");
+let buy_button12 = document.getElementById("buy_button12");
+let buy_button13 = document.getElementById("buy_button13");
+let buy_button14 = document.getElementById("buy_button14");
+let buy_button15 = document.getElementById("buy_button15");
+let buy_button16 = document.getElementById("buy_button16");
+
+//endi tugmalarni eshitamiz
+buy_button1.addEventListener("click", async () => {
+  let result = await confirm(
+    "Siz ushbu mahsulotni coinlarga almashtirmoqchimisiz?",
+  );
+  if (result) {
+    alert(
+      "Siz ushbu mahsulotni coinlarga almashtirish uchun ushbu matnni kuratorga yuborng: Assalomu aleykum ustoz, men 'Kepka' mahsulotini coinlarga almashtirmoqchiman. ",
+    );
+  } else {
+    alert("Siz ushbu mahsulotni coinlarga almashtirmoqchi emassiz");
+  }
+});
+
+buy_button2.addEventListener("click", async () => {
+  let result = await confirm(
+    "Siz ushbu mahsulotni coinlarga almashtirmoqchimisiz?",
+  );
+  if (result) {
+    alert(
+      "Siz ushbu mahsulotni coinlarga almashtirish uchun ushbu matnni kuratorga yuborng: Assalomu aleykum ustoz, men 'Simsiz quloqchin pro 5c' mahsulotini coinlarga almashtirmoqchiman. ",
+    );
+  } else {
+    alert("Siz ushbu mahsulotni coinlarga almashtirmoqchi emassiz");
+  }
+});
+
+buy_button3.addEventListener("click", async () => {
+  let result = await confirm(
+    "Siz ushbu mahsulotni coinlarga almashtirmoqchimisiz?",
+  );
+  if (result) {
+    alert(
+      "Siz ushbu mahsulotni coinlarga almashtirish uchun ushbu matnni kuratorga yuborng: Assalomu aleykum ustoz, men 'Termos' mahsulotini coinlarga almashtirmoqchiman. ",
+    );
+  } else {
+    alert("Siz ushbu mahsulotni coinlarga almashtirmoqchi emassiz");
+  }
+});
+
+buy_button4.addEventListener("click", async () => {
+  let result = await confirm(
+    "Siz ushbu mahsulotni coinlarga almashtirmoqchimisiz?",
+  );
+  if (result) {
+    alert(
+      "Siz ushbu mahsulotni coinlarga almashtirish uchun ushbu matnni kuratorga yuborng: Assalomu aleykum ustoz, men 'Komyuter uchun ko'zoynak' mahsulotini coinlarga almashtirmoqchiman. ",
+    );
+  } else {
+    alert("Siz ushbu mahsulotni coinlarga almashtirmoqchi emassiz");
+  }
+});
+
+buy_button5.addEventListener("click", async () => {
+  let result = await confirm(
+    "Siz ushbu mahsulotni coinlarga almashtirmoqchimisiz?",
+  );
+  if (result) {
+    alert(
+      "Siz ushbu mahsulotni coinlarga almashtirish uchun ushbu matnni kuratorga yuborng: Assalomu aleykum ustoz, men 'Simsiz quloqchin M28' mahsulotini coinlarga almashtirmoqchiman. ",
+    );
+  } else {
+    alert("Siz ushbu mahsulotni coinlarga almashtirmoqchi emassiz");
+  }
+});
+
+buy_button6.addEventListener("click", async () => {
+  let result = await confirm(
+    "Siz ushbu mahsulotni coinlarga almashtirmoqchimisiz?",
+  );
+  if (result) {
+    alert(
+      "Siz ushbu mahsulotni coinlarga almashtirish uchun ushbu matnni kuratorga yuborng: Assalomu aleykum ustoz, men 'Smart watch 8Big T800 pro max' mahsulotini coinlarga almashtirmoqchiman. ",
+    );
+  } else {
+    alert("Siz ushbu mahsulotni coinlarga almashtirmoqchi emassiz");
+  }
+});
+
+buy_button7.addEventListener("click", async () => {
+  let result = await confirm(
+    "Siz ushbu mahsulotni coinlarga almashtirmoqchimisiz?",
+  );
+  if (result) {
+    alert(
+      "Siz ushbu mahsulotni coinlarga almashtirish uchun ushbu matnni kuratorga yuborng: Assalomu aleykum ustoz, men 'Ryukzak noutbuk uchun(USB)' mahsulotini coinlarga almashtirmoqchiman. ",
+    );
+  } else {
+    alert("Siz ushbu mahsulotni coinlarga almashtirmoqchi emassiz");
+  }
+});
+
+buy_button8.addEventListener("click", async () => {
+  let result = await confirm(
+    "Siz ushbu mahsulotni coinlarga almashtirmoqchimisiz?",
+  );
+  if (result) {
+    alert(
+      "Siz ushbu mahsulotni coinlarga almashtirish uchun ushbu matnni kuratorga yuborng: Assalomu aleykum ustoz, men 'VR ochki shinecon 6.0' mahsulotini coinlarga almashtirmoqchiman. ",
+    );
+  } else {
+    alert("Siz ushbu mahsulotni coinlarga almashtirmoqchi emassiz");
+  }
+});
+
+buy_button9.addEventListener("click", async () => {
+  let result = await confirm(
+    "Siz ushbu mahsulotni coinlarga almashtirmoqchimisiz?",
+  );
+  if (result) {
+    alert(
+      "Siz ushbu mahsulotni coinlarga almashtirish uchun ushbu matnni kuratorga yuborng: Assalomu aleykum ustoz, men 'Klavbord va sichqoncha kombo' mahsulotini coinlarga almashtirmoqchiman. ",
+    );
+  } else {
+    alert("Siz ushbu mahsulotni coinlarga almashtirmoqchi emassiz");
+  }
+});
+
+buy_button10.addEventListener("click", async () => {
+  let result = await confirm(
+    "Siz ushbu mahsulotni coinlarga almashtirmoqchimisiz?",
+  );
+  if (result) {
+    alert(
+      "Siz ushbu mahsulotni coinlarga almashtirish uchun ushbu matnni kuratorga yuborng: Assalomu aleykum ustoz, men 'Air Pods Max' mahsulotini coinlarga almashtirmoqchiman. ",
+    );
+  } else {
+    alert("Siz ushbu mahsulotni coinlarga almashtirmoqchi emassiz");
+  }
+});
+
+buy_button11.addEventListener("click", async () => {
+  let result = await confirm(
+    "Siz ushbu mahsulotni coinlarga almashtirmoqchimisiz?",
+  );
+  if (result) {
+    alert(
+      "Siz ushbu mahsulotni coinlarga almashtirish uchun ushbu matnni kuratorga yuborng: Assalomu aleykum ustoz, men 'Yandex stansiya(alisa)' mahsulotini coinlarga almashtirmoqchiman.  ",
+    );
+  } else {
+    alert("Siz ushbu mahsulotni coinlarga almashtirmoqchi emassiz");
+  }
+});
+
+buy_button12.addEventListener("click", async () => {
+  let result = await confirm(
+    "Siz ushbu mahsulotni coinlarga almashtirmoqchimisiz?",
+  );
+  if (result) {
+    alert(
+      "Siz ushbu mahsulotni coinlarga almashtirish uchun ushbu matnni kuratorga yuborng: Assalomu aleykum ustoz, men 'Xiaomi Redmi 9A' mahsulotini coinlarga almashtirmoqchiman. ",
+    );
+  } else {
+    alert("Siz ushbu mahsulotni coinlarga almashtirmoqchi emassiz");
+  }
+});
+
+buy_button13.addEventListener("click", async () => {
+  let result = await confirm(
+    "Siz ushbu mahsulotni coinlarga almashtirmoqchimisiz?",
+  );
+  if (result) {
+    alert(
+      "Siz ushbu mahsulotni coinlarga almashtirish uchun ushbu matnni kuratorga yuborng: Assalomu aleykum ustoz, men 'Huawei MatePad SE plansheti' mahsulotini coinlarga almashtirmoqchiman.",
+    );
+  } else {
+    alert("Siz ushbu mahsulotni coinlarga almashtirmoqchi emassiz");
+  }
+});
+
+buy_button14.addEventListener("click", async () => {
+  let result = await confirm(
+    "Siz ushbu mahsulotni coinlarga almashtirmoqchimisiz?",
+  );
+  if (result) {
+    alert(
+      "Siz ushbu mahsulotni coinlarga almashtirish uchun ushbu matnni kuratorga yuborng: Assalomu aleykum ustoz, men 'Noutbuk Lenevo' mahsulotini coinlarga almashtirmoqchiman. ",
+    );
+  } else {
+    alert("Siz ushbu mahsulotni coinlarga almashtirmoqchi emassiz");
+  }
+});
+
+buy_button15.addEventListener("click", async () => {
+  let result = await confirm(
+    "Siz ushbu mahsulotni coinlarga almashtirmoqchimisiz?",
+  );
+  if (result) {
+    alert(
+      "Siz ushbu mahsulotni coinlarga almashtirish uchun ushbu matnni kuratorga yuborng: Assalomu aleykum ustoz, men 'Macbook Air 13' mahsulotini coinlarga almashtirmoqchiman. ",
+    );
+  } else {
+    alert("Siz ushbu mahsulotni coinlarga almashtirmoqchi emassiz");
+  }
+});
+
+buy_button16.addEventListener("click", async () => {
+  let result = await confirm(
+    "Siz ushbu mahsulotni coinlarga almashtirmoqchimisiz?",
+  );
+  if (result) {
+    alert(
+      "Siz ushbu mahsulotni coinlarga almashtirish uchun ushbu matnni kuratorga yuborng: Assalomu aleykum ustoz, men 'Iphone 14 pro' mahsulotini coinlarga almashtirmoqchiman. ",
+    );
+  } else {
+    alert("Siz ushbu mahsulotni coinlarga almashtirmoqchi emassiz");
+  }
+});
